55fd416868f58f9af2c72e1dc7250f15c8526bc8
githubのユーザー名 <github>
2024-11-08 06:26:10 +0000

    Deploy post controller by Zk

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+use App\Http\Controllers\PostController;
+Route::get('/posts', [PostController::class, 'index']);
 
 Route::get('/', function () {
     return view('welcome');
