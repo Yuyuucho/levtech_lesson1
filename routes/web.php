02d765e915da554d2be1245e8adefe1c44280312
githubのユーザー名 <github>
2024-11-08 14:21:34 +0000

    add mvc system and pagination (Lesson7-2) by Zk

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 use App\Http\Controllers\PostController;
-Route::get('/posts', [PostController::class, 'index']);
+Route::get('/', [PostController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
+/*Route::get('/', function () {
+    return view('posts.index');
 });
+*/
+?>
